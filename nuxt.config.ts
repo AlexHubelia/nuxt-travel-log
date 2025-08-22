541a0ee12affd7e51100ee7587816391a8aea74b
AlexHubelia <alexandre.gilbert@hubelia.com>
2025-08-21 21:23:42 -0400

    Revert "setup eslint / lint-staged"
    This reverts commit 2e3df7035b59f0896a5618522c15fe8acb7d8898.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config";
-
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
-  eslint: {
-    config: {
-      standalone: false,
-    },
-  },
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
