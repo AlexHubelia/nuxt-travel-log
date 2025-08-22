374e78014760979a5de5a734511c8b05d7b3d74e
AlexHubelia <alexandre.gilbert@hubelia.com>
2025-08-21 22:46:43 -0400

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config";
-
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   modules: ["@nuxt/eslint"],
+
   eslint: {
     config: {
       standalone: false,
